3699805ac6f4b4c12e4c2a6e137e1f685091a4df
Konstantin <humbl3dev@gmail.com>
2023-09-14 16:24:57 -0700

    bring tailwind theme config and fonts from production site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,13 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Poppins:wght@600;700&display=swap"
+	/>
+</svelte:head>
+
 <main>
 	<slot />
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: '#430ebe',
+				primaryDark: '#240865',
+				primaryLighter: '#5a1bee',
+				primaryLight: '#eff2ff',
+				secondary: '#a4133c',
+				secondaryLighter: '#c9184a',
+				secondaryLight: '#ffccd5',
+				accent: '#FDC500',
+				accentDark: '#E0B000',
+				accentLight: '#FFE999'
+			}
+		},
+		fontFamily: {
+			serif: ['Poppins', ...defaultTheme.fontFamily.serif],
+			sans: ['Inter', ...defaultTheme.fontFamily.sans]
+		}
 	},
 	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
 };
