094d5470cc1103617492fdc5795e06b50c396b0b
Konstantin <humbl3dev@gmail.com>
2023-09-29 17:23:29 -0700

    add contact us submission error

---
+++ src/routes/contact/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+	import PageHeader from '$lib/components/layout/PageHeader.svelte';
+</script>
+
+<PageHeader title="Contact Us" />
+<section
+	class="mx-auto my-6 max-w-screen-md rounded-md bg-white p-6 shadow-sm drop-shadow-sm sm:my-12"
+>
+	<h1 class="mb-4 text-2xl font-semibold">Submission error</h1>
+	<p class="text-red-600">We are unable to process your request. Please try again later.</p>
+</section>
