aad0763d54b2f2d2b62907eb38967c364dbc9bc9
Konstantin <humbl3dev@gmail.com>
2023-09-29 17:21:54 -0700

    Update page header component to include option for transparency

--- src/lib/components/layout/PageHeader.svelte
+++ src/lib/components/layout/PageHeader.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	export let title: string;
-	export let subtext = '';
+	export let subtext: string | null = null;
+	export let transparent = false;
 </script>
 
-<header class="bg-primaryDark py-20 text-center">
-	<h1 class="mb-6 text-4xl text-white md:text-5xl">{title}</h1>
+<header class="{transparent ? '' : 'bg-primaryDark'} py-20 text-center">
+	<h1 class="mb-6 text-4xl {transparent ? '' : 'text-white'} md:text-5xl">{title}</h1>
 	{#if subtext}
-		<p class="mx-auto max-w-lg px-4 text-gray-100">{subtext}</p>
+		<p class="mx-auto max-w-lg px-4 {transparent ? '' : 'text-gray-100'}">{subtext}</p>
 	{/if}
 </header>
