b99bff8ce63f53b3e6280d04773a71e485beac0a
Konstantin <humbl3dev@gmail.com>
2023-10-02 11:46:03 -0700

    project detail page updates

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,19 +3,30 @@
 	import { buildUrlFromImageSource } from '$lib/sanity/imageUrlBuilder.js';
 
 	export let data;
+	const imageWidth = 1024;
+	const imageHeight = 800;
 </script>
 
 <PageHeader title="Projects" />
 <div class="page-content">
 	<div class="grid grid-cols-1 gap-8 py-8 md:grid-cols-2 lg:grid-cols-3">
 		{#each data.projects as project}
-			{@const imageUrl = buildUrlFromImageSource(project.thumb, { width: 1000, height: 600 })}
+			{@const imageUrl = buildUrlFromImageSource(project.thumb, {
+				width: imageWidth,
+				height: imageHeight
+			})}
 			<a
 				href="/projects/{project.slug}"
 				class="group opacity-90 transition-opacity duration-300 hover:opacity-100"
 			>
 				<div class="mb-4 overflow-hidden rounded-md shadow-sm drop-shadow-sm">
-					<img src={imageUrl} alt={project.name} loading="lazy" />
+					<img
+						src={imageUrl}
+						alt={project.name}
+						loading="lazy"
+						width={imageWidth}
+						height={imageHeight}
+					/>
 				</div>
 				<div class="flex items-center gap-2 text-xl font-bold">
 					{project.name}
--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -14,34 +14,40 @@
 </script>
 
 <section class="wrapper my-12">
-	<a
-		href="/projects"
-		class="mb-4 inline-block text-gray-800 underline hover:text-gray-700 hover:no-underline"
-	>
-		&larr; All Projects
-	</a>
 	<div class="grid gap-10 lg:grid-cols-2">
-		<div class="relative h-auto overflow-hidden rounded-md shadow-2xl sm:max-h-max">
-			<div class="absolute inset-0 z-[-1] h-full w-full bg-slate-300" />
+		<div class="relative h-auto rounded-md sm:max-h-max">
 			<img
-				class="max-w-full sm:object-cover"
+				class="max-w-full"
 				width={projectImageWidth}
 				height={projectImageHeight}
 				src={projectImageUrl}
 				alt={project.name}
 			/>
 		</div>
-		<div>
+		<div class="@container">
 			<h1 class="mb-4 text-4xl">{project.name}</h1>
 			<p>{project.description}</p>
-			<a
-				href={project.url}
-				target="_blank"
-				class="button mt-8 inline-flex items-center gap-2"
-				rel="noreferrer noopener"
-			>
-				View this project <Icon icon="mdi:open-in-new" class="text-2xl" />
-			</a>
+			<div class="mt-8 grid grid-cols-1 gap-4 @lg:grid-cols-2">
+				<a href="/projects" class="button button--ghost button--withIcon">
+					<Icon icon="radix-icons:pin-left" class="text-2xl" /> View All Projects
+				</a>
+				<a
+					href={project.url}
+					target="_blank"
+					class="button button--withIcon"
+					rel="noreferrer noopener"
+				>
+					View this project <span class="link-icon-wrapper">
+						<Icon icon="ph:link-light" class="text-2xl" />
+					</span>
+				</a>
+			</div>
 		</div>
 	</div>
 </section>
+
+<style>
+	.link-icon-wrapper {
+		transform: rotate(0.2turn);
+	}
+</style>
