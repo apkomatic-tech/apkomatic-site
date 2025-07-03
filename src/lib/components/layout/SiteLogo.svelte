0ee3fc2de3732bae74d493c838a0339b3569f086
Konstantin <humbl3dev@gmail.com>
2025-07-03 15:48:13 -0700

    Update aria-hidden attribute in SiteLogo component for improved accessibility

--- src/lib/components/layout/SiteLogo.svelte
+++ src/lib/components/layout/SiteLogo.svelte
@@ -9,7 +9,7 @@
 	<img src={logo} class="inline-block h-16 w-16" alt="Apkomatic Logo" />
 	<span class="ml-[5px] text-[1.6rem] text-primary md:text-3xl" aria-hidden> pkomatic </span>
 	<span
-		aria-hidden
+		aria-hidden="true"
 		class="ml-[3px] inline-block translate-y-[1px] text-[1.3rem] lowercase text-accentDark md:translate-y-[3px]"
 		>Tech</span
 	>
